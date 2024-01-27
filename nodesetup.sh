c101dcbe750cbd16aee119f8d340b9395d815677
LL-Etiane <lukongleinyuyetiane@gmail.com>
2024-01-27 04:35:44 +0100

    adds script

---
+++ nodesetup.sh
@@ -0,0 +1,6 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+npm install
+npm i @esbuild/linux-x64
+npm run build
