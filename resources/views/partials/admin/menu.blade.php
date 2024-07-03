a1d749dbf8bb315bbd1afba83b8cd54dffd20883
Hermann <wambahermann084@gmail.com>
2024-07-03 09:21:19 +0100

    readjust menu

--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -1208,8 +1208,8 @@
         
                         <li
                             class="dash-item dash-hasmenu {{ Request::segment(1) == 'email_template' || Request::route()->getName() == 'manage.email.language' ? ' active dash-trigger' : 'collapsed' }}">
-                            <a href="#"
-                            {{-- <a href="{{ route('manage.email.language', [$emailTemplate->id, \Auth::user()->lang]) }}" --}}
+                            {{-- <a href="#" --}}
+                            <a href="{{ route('manage.email.language', [$emailTemplate->id, \Auth::user()->lang]) }}"
                                 class="dash-link">
                                 <span class="dash-mtext">{{ __('Email Template') }}</span></a>
                         </li>
